6c10cb112fb0e2d677d7b410a90e0c5100afea55
이예슬 <43533905+2yeseul@users.noreply.github.com>
2021-10-05 17:30:43 +0900

    update

---
+++ src/play/HelloWorld.kt
@@ -0,0 +1,13 @@
+package play
+
+fun printMessage(message: String): Unit {
+    println(message)
+}
+
+fun printMessageWithPrefix(message: String, prefix: String = "Info") {
+    println("[$prefix] $message")
+}
+
+fun main() {
+    println("Hello World")
+}
